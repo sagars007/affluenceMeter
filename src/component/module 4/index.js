e08784873c0c45e48a4b92f5c186638cf6643df8
madhav <madhav@madhavs-MacBook-Pro-2.local>
2021-09-28 12:41:14 +0530

    linked mod 4.2

--- src/component/module 4/index.js
+++ src/component/module 4/index.js
@@ -14,6 +14,7 @@ import {
   LargeConatiner,
 } from "../styles/app.styles";
 import { GeneralDiv } from "../styles/education.styles";
+import YoutubeEmbed from "../YouTube";
 import {
   filterBand,
   graph1finalAxis,
